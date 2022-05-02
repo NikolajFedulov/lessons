edd0667bbb3e1918295c7cb156aadfdc28f6d913
Nikolaj Fedulov <v20nickf1@gmail.com>
2022-05-02 20:47:23 +0300

    Using charAt(int ind)

--- src/main/java/lesson11/Jewels.java
+++ src/main/java/lesson11/Jewels.java
@@ -5,12 +5,9 @@ public class Jewels {
     public int numberOfJewels(String jewels, String stones) {
         int count = 0;
         if (jewels!=null && stones!=null) {
-            char[] jewelsArray = jewels.toCharArray();
-            char[] stonesArray = stones.toCharArray();
-
-            for (int i=0; i<jewelsArray.length; i++) {
-                for (int j=0; j<stonesArray.length; j++) {
-                    if (jewelsArray[i] == stonesArray[j]) {
+            for (int i=0; i<jewels.length(); i++) {
+                for (int j=0; j<stones.length(); j++) {
+                    if (jewels.charAt(i) == stones.charAt(j)) {
                         count++;
                     }
                 }
