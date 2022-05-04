86942e3a876013fe47d6f3553ad75b7516a79f14
Nikolaj Fedulov <v20nickf1@gmail.com>
2022-05-04 21:56:02 +0300

    some changes in try, catch, throw

--- src/main/java/lesson13/Lesson13Task.java
+++ src/main/java/lesson13/Lesson13Task.java
@@ -5,29 +5,16 @@ import java.util.Arrays;
 public class Lesson13Task {
 
     public int productOfMaximalNumbers(int[] intsArray) throws ArrayIndexOutOfBoundsException, NullPointerException {
-        try {
-            Arrays.sort(intsArray);
-            return (intsArray[intsArray.length-1]-1) * (intsArray[intsArray.length-2]-1);
-        }
-        catch (ArrayIndexOutOfBoundsException e) {
-            throw e;
-        }
-        catch (NullPointerException e) {
-            throw e;
-        }
+        Arrays.sort(intsArray);
+        return (intsArray[intsArray.length-1]-1) * (intsArray[intsArray.length-2]-1);
     }
 
     public int[] arrayOfTheSquares(int[] intsArray) throws NullPointerException{
-        try {
-            Arrays.sort(intsArray);
-            for (int i=0; i<intsArray.length; i++) {
-                intsArray[i] *= intsArray[i];
-            }
-            Arrays.sort(intsArray);
-            return intsArray;
+        Arrays.sort(intsArray);
+        for (int i=0; i<intsArray.length; i++) {
+            intsArray[i] *= intsArray[i];
         }
-        catch (NullPointerException e) {
-            throw e;
-        }
+        Arrays.sort(intsArray);
+        return intsArray;
     }
 }
