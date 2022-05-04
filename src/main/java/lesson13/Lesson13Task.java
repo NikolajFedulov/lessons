ee4fa0f5f20658550fe70fb0d2cf79c1ce7fd207
Nikolaj Fedulov <v20nickf1@gmail.com>
2022-05-04 21:50:43 +0300

    Initial commit for lesson # 13

---
+++ src/main/java/lesson13/Lesson13Task.java
@@ -0,0 +1,33 @@
+package lesson13;
+
+import java.util.Arrays;
+
+public class Lesson13Task {
+
+    public int productOfMaximalNumbers(int[] intsArray) throws ArrayIndexOutOfBoundsException, NullPointerException {
+        try {
+            Arrays.sort(intsArray);
+            return (intsArray[intsArray.length-1]-1) * (intsArray[intsArray.length-2]-1);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            throw e;
+        }
+        catch (NullPointerException e) {
+            throw e;
+        }
+    }
+
+    public int[] arrayOfTheSquares(int[] intsArray) throws NullPointerException{
+        try {
+            Arrays.sort(intsArray);
+            for (int i=0; i<intsArray.length; i++) {
+                intsArray[i] *= intsArray[i];
+            }
+            Arrays.sort(intsArray);
+            return intsArray;
+        }
+        catch (NullPointerException e) {
+            throw e;
+        }
+    }
+}
